7d33a344b01c422b0dc06527a82d9c7d5ee2fad1
anynines-johannchopin <jchopin@anynines.com>
2020-10-06 16:26:57 +0200

    🔧 Serve mocked API

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,8 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
+    contentBase: [path.join(__dirname, 'public/'), path.join(__dirname, 'src/mockedApi/')],
+    contentBasePublicPath: ['/', '/api'],
     compress: true,
     writeToDisk: true,
     port: 9000
