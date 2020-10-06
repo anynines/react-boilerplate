b4e840f96238a3848984b101a711158bd01eff1a
anynines-johannchopin <jchopin@anynines.com>
2020-10-06 16:17:07 +0200

    ✅ App for the demo

---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import 'jest-styled-components'
+
+// C O M P O N E N T S
+import App from './App'
+import { Icon } from '@avarteqgmbh/design_system'
+
+describe('<App />', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(<App />)
+  })
+
+  it('should contain a title', () => {
+    expect(wrapper.find('h1').exists()).toBe(true)
+  })
+
+  it('should contain an Icon', () => {
+    expect(wrapper.find(Icon).exists()).toBe(true)
+  })
+})
