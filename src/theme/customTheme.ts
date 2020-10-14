164cc879e00c9680b6537a5c088b6bb3e7c0f442
anynines-johannchopin <jchopin@anynines.com>
2020-10-14 10:27:10 +0200

    ✨ Add createGlobalStyle

--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -1,9 +1,10 @@
-import { PartialTheme } from '@anynines/design-system'
+import { PartialTheme, Theme as DesignSystemTheme } from '@anynines/design-system'
 
 // I N T E R F A C E S
 export interface CustomTheme extends PartialTheme {
   customColor: string
 }
+export type Theme = CustomTheme & DesignSystemTheme
 
 export const CUSTOM_THEME: CustomTheme = {
   colors: {
